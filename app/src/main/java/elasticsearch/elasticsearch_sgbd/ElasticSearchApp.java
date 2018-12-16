a79bcd6309b3d0497bbd9f7c17c9a9846f460e5b
magalenyo <magalenyo7@gmail.com>
2018-12-16 18:53:49 +0100

    Complete push

--- app/src/main/java/elasticsearch/elasticsearch_sgbd/ElasticSearchApp.java
+++ app/src/main/java/elasticsearch/elasticsearch_sgbd/ElasticSearchApp.java
@@ -10,7 +10,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ElasticSearchApp extends Application {
 
-    //static String API_BASE_URL = "localhost:9200";
+    //static String API_BASE_URL = "http://localhost:9200";
     static String API_BASE_URL = "http://192.168.1.34:9200";
 
     private ElasticSearchAPI restAPI;
